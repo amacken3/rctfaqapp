15a79efded79f3e3fd5688e78f2d71f62c9ea032
amacken3 <aengus.2000@gmail.com>
2024-09-06 17:44:48 -0400

    removed js file taht is unnecessary

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,7 +4,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
 import Dashboard from './components/Dashboard';
-import ReplitBackEnd from './components/ReplitBackEnd';  // Import ReplitBackEnd
+
 
 const App = () => {
   return (
@@ -14,7 +14,6 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/replit-backend" element={<ReplitBackEnd />} /> {/* Add ReplitBackEnd route */}
       </Routes>
     </Router>
   );
